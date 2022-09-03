2fc3f3ccc52f1ed3b642377e56c960f452aae7be
Dykyi Maksym <108894931+DykyiMaxim@users.noreply.github.com>
2022-09-02 21:02:08 -0300

    Working on Location Tracking

--- app/src/main/java/com/WM/runny/presentation/RunFragment/RunFragment.kt
+++ app/src/main/java/com/WM/runny/presentation/RunFragment/RunFragment.kt
@@ -3,10 +3,20 @@ package com.WM.runny.presentation.RunFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.WM.runny.R
+import com.WM.runny.common.TrackingUtility
 import com.WM.runny.presentation.MainScreen.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RunFragment:Fragment(R.layout.fragmet_run) {
     private val viewModel:MainViewModel by viewModels()
+
+
+    private fun RequestPermissiom(){
+        if(TrackingUtility.hasLocationPermission(requireContext())){
+            return
+
+        }
+
+    }
 }
